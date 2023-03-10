61da84ddfba7ac4a15f40c61ee4295b654147d5e
anowak-hub <72890611+anowak-hub@users.noreply.github.com>
2023-03-09 21:08:14 -0600

    Completed PATCH route of editing an activity

--- backend/src/activity-routes.js
+++ backend/src/activity-routes.js
@@ -78,14 +78,111 @@ router.post("/", async (req, res) => {
 });
 
 // Update a specific Activity object
-router.patch("/:activityId", async (req, res) => {
+router.patch("/:activityId", async (req, res, next) => {
     // --- YOUR CODE GOES UNDER THIS LINE --- 
+    const activityId = req.params.activityId;
 
-    // --------- DELETE THIS CONTENT --------
-    res.send({
-        message: "Hello World"
-    })
-    // -------------------------------------
+    const name = req.body.name;
+    const categories = req.body.categories;
+    const description = req.body.description;
+    const meetingDays = req.body.meetingDays;
+    const meetingStartEndTimes = req.body.meetingStartEndTimes;
+    const location = req.body.location;
+    const link = req.body.link;
+    const displayedPublically = req.body.displayedPublically;
+
+    Activity.findById(activityId)
+        .then(activity => {
+            if (!activity) {
+                const error = new Error('Error: Could not find the activity.');
+                error.statusCode = 404;
+                throw error;
+            }
+
+            activity.name = name;
+            activity.categories = categories;
+            activity.description = description;
+            activity.meetingDays = meetingDays;
+            activity.meetingStartEndTimes = meetingStartEndTimes;
+            activity.location = location;
+            activity.link = link;
+            activity.displayedPublically = displayedPublically;
+
+            if (activity.name === "") {
+                const error = new Error('Please enter a name.');
+                error.statusCode = 404;
+                throw error;
+            }  
+            
+            if (activity.categories.length === 0) {
+                const error = new Error('Please enter at least one category (Empty category array).');
+                error.statusCode = 404;
+                throw error;
+            }
+
+            for(let i = 0; i < activity.categories.length; i++) {
+                if (activity.categories[i] === "") {
+                    const error = new Error('Please fill each category accordingly (Left a category blank).');
+                    error.statusCode = 404;
+                    throw error;
+                }
+            }
+
+            if (activity.description === "") {
+                const error = new Error('Please enter a description.');
+                error.statusCode = 404;
+                throw error;
+            }
+
+            if (activity.meetingDays.length === 0) {
+                const error = new Error('Please enter at least one meeting day (Empty meeting day array).');
+                error.statusCode = 404;
+                throw error;
+            }
+
+            for(let i = 0; i < activity.meetingDays.length; i++) {
+                if (activity.meetingDays[i] === "") {
+                    const error = new Error('Please fill each meeting day accordingly (Left a meeting day blank).');
+                    error.statusCode = 404;
+                    throw error;
+                }
+            }
+
+            if (activity.meetingStartEndTimes === "") {
+                const error = new Error('Please enter the time the meeting starts and ends.');
+                error.statusCode = 404;
+                throw error;
+            }
+
+            if (activity.location === "") {
+                const error = new Error('Please enter a location.');
+                error.statusCode = 404;
+                throw error;
+            }
+
+            if (activity.link === "") {
+                const error = new Error('Please enter a link.');
+                error.statusCode = 404;
+                throw error;
+            }
+
+            if (activity.displayedPublically !== true && activity.displayedPublically !== false) {
+                const error = new Error('Please enter whether or not the activity should be displayed publicly.');
+                error.statusCode = 404;
+                throw error;
+            }
+
+            return activity.save();
+        })
+        .then(result => {
+            res.status(200).json({message: 'Activity Updated', activity: result});
+        })
+        .catch(err => {
+            if(!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
 });
 
 // Delete a specific Activity object
