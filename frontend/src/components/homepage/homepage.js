9c4cb4e6e8e548b05268da242fe9ec5a090022dd
Alex Strugacz <90818590+Nectar0@users.noreply.github.com>
2023-03-11 17:53:22 -0600

    added tailwind and configured it

--- frontend/src/components/homepage/homepage.js
+++ frontend/src/components/homepage/homepage.js
@@ -2,6 +2,9 @@ function Search() {
   return (
     <div className="">
       <h1>I'm in search!</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world! in search, using tailwind
+      </h1>
     </div>
   );
 }
