9cb59268e28e3f24f414e6b61e80aed9e6a20fc8
RomanWKus <126989081+RomanWKus@users.noreply.github.com>
2023-04-22 14:55:04 -0500

    Rendered club activities

--- frontend/src/components/StaffHome/StaffClub.js
+++ frontend/src/components/StaffHome/StaffClub.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-const StaffClub = (props) => {
-    
-    return (
-        <div className="text-black mt-5 bg-box phone: w-9/10 tablet:w-9/10 laptop:w-749px desktop: w-749px">
+const StaffClub = ({ sub, activities }) => {
+  return (
+    <div className="text-black mt-5 bg-box phone: w-9/10 tablet:w-9/10 laptop:w-749px desktop: w-749px">
       <div style={{ display: "inline-block" }}>
         <h2 className="subheader-font ml-1" style={{ display: "inline-block" }}>
-          {props.sub}
+          {sub}
         </h2>
+        <ul>
+          {activities.map((activity) => (
+            <li key={activity._id}>{activity.name}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
