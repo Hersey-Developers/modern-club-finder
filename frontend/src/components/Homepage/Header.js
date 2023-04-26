81fd4dd3474db41798a5eb9a5581a47ef8303334
Alexander Masztak <itzalqxander@gmail.com>
2023-04-26 18:09:51 -0500

    buttons

--- frontend/src/components/Homepage/Header.js
+++ frontend/src/components/Homepage/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
       <h1 className="header-font">Hersey Club Finder</h1>
       <h2 className="header-description-font mb-4">
-        Find your next Hersey club!
+        Find your next Hersey club!{" "}
+        <span style={{ fontWeight: "bold" }}>Click </span>
+        <Link to="/stafflogin" className="btn btn-primary">
+          <u><span style={{ fontWeight: "bold" }}>here</span></u>
+        </Link>
+        <span style={{ fontWeight: "bold" }}> for Staff Login page.</span>
       </h2>
     </div>
   );
