84a3553e95a0189e54ebd62898ea9c0ffddf482c
JimmyDesu <finneganjimmy0@gmail.com>
2023-05-06 14:57:12 -0500

    mobile optimization

--- frontend/src/components/StaffHome/StaffClub.js
+++ frontend/src/components/StaffHome/StaffClub.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const StaffClub = ({ sub, activities }) => {
   return (
-    <div className="text-black mt-5 bg-box phone: w-9/10 tablet:w-9/10 laptop:w-749px desktop: w-749px">
+    <div className="text-black mt-5 bg-box phone: w-9/10 tablet:w-9/10 laptop:w-749px desktop: w-749px" style={{ overflow: 'auto', overflowWrap: 'break-word' }}>
       <div style={{ display: "inline-block" }}>
         <h2 className="subheader-font ml-1" style={{ display: "inline-block" }}>
           {sub}
